46506f15d6937948ef7b233cc246d24ecf2d1e9e
Johnson <johnson.ezmail@gmail.com>
2025-05-19 11:03:45 +0800

    Init

---
+++ Models/Tables/TableRelations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace mvcdemo9.Models;
+
+public partial class TableRelations
+{
+    public int Id { get; set; }
+
+    public string? RelationName { get; set; }
+
+    public string? LeftTableName { get; set; }
+
+    public string? RightTableName { get; set; }
+
+    public string? LeftColumnName { get; set; }
+
+    public string? LeftDataType { get; set; }
+
+    public string? RightColumnName { get; set; }
+
+    public string? RightDataType { get; set; }
+
+    public string? RelationType { get; set; }
+
+    public string? Remark { get; set; }
+}
